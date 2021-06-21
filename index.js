e8b8406137f2f0fad5358421bd604bac371ac534
Anna Wilson <ajoyna01@gmail.com>
2021-06-21 15:23:01 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  //function needs to get the 5th book
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
+   
+    
   // To pass the tests, don't forget to return your fetch!
   
 }
@@ -7,7 +13,9 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    const h1 = document.createElement('h1');
+    h1.innerText = book.character;
+    h2.innerText = book.name;
     main.appendChild(h2);
   });
 }
